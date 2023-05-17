c75ce9e6e972ae73caf26cc13396ed34ea79e68a
Angeluz99 <122189395+Angeluz99@users.noreply.github.com>
2023-05-17 10:03:00 -0600

    Add files via upload

--- CultoText.js
+++ CultoText.js
@@ -1,12 +1,5 @@
 
-//putting Culto image on map
-
-  var optionsDiv = document.getElementById("optionCulto2B");
-  optionsDiv.addEventListener("click", function() {
-    // Get a reference to the "Culto" div in the parent window
-    var cultoDiv = window.opener.document.getElementById("youngwoman");
-    cultoDiv.style.diplay = "block";   
-    });
+//borrar
 
 
 
